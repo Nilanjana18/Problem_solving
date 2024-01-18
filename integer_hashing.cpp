1565bdc846df976df3ecb1a73650d23411e3d3a8
Nilanjana Roy <nilanjanaroy524@gmail.com>
2024-01-18 11:37:14 +0630

    Create integer_hashing.cpp

---
+++ integer_hashing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    const int MAX_SIZE = 100000; // Set an appropriate maximum size
+    vector<int> hash(MAX_SIZE, 0);
+
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]] += 1;
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        cout << hash[number] << endl;
+    }
+
+    return 0;
+}
